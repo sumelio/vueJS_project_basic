f2cba8f4590acd1a93ec4d77283233c5add63991
sumel <sumel124@gmail.com>
2017-11-02 19:15:50 -0500

    Image and component artist

---
+++ music/platzimusic/src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+	appname: 'platzimusic',
+	apiKey: '2fba613076ced498a0e5d313be21596a',
+	secret: 'ea978f2d5e9cd9535623bcb2c8a067b0',
+	RegisteredTo: 'soyellemus*/'
+}
+
+/*Here are the details of your new API account.
+Application name	platzimusic
+API key	2fba613076ced498a0e5d313be21596a
+Shared secret	ea978f2d5e9cd9535623bcb2c8a067b0
+Registered to	soyellemus*/
