ed95a244f45e463b57cde5c483e9d908e75fffe3
sumel <sumel124@gmail.com>
2017-11-02 20:35:28 -0500

    update url

--- music/platzimusic/src/App.vue
+++ music/platzimusic/src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://github.com/sumelio/vueJS_project_basic/tree/master/music/platzimusic/logo.png')
     h1 PlatziMusic
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value" ) {{ country.name }}
